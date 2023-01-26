a66d515c1d718be98a386606684661712c89189a
+ <+>
2023-01-26 17:28:32 +0300

    updated components

--- tech-blog/src/App.js
+++ tech-blog/src/App.js
@@ -1,16 +1,33 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
-import { Routes, Route } from 'react-router-config';
-
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create" element={<Create />} />
-   </Routes>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
